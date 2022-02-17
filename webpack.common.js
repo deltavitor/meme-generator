4230e40994cb589924e258d148dbb38a4559d3ab
deltavitor <cvitor1618@gmail.com>
2022-02-17 18:00:29 -0300

    Created project

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ]
+}
