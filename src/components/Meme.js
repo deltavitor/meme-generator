18bc6f481637d65a3f257c41e11e5b3cfbce0734
deltavitor <cvitor1618@gmail.com>
2022-02-19 13:32:10 -0300

    Added some initial functionality to the genereate a meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,19 @@
 import React from "react"
 
+import memesData from "../memesData.js"
+
 function Meme() {
-  return(
+  function generateMemeImage() {
+    const memeId = Math.floor(Math.random() * memesData.data.memes.length)
+    const imgUrl = memesData.data.memes[memeId].url
+    console.log(imgUrl)
+  }
+  return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input"></input>
         <input type="text" placeholder="Bottom text" className="form--input"></input>
-        <button className="form--submit">Get a new meme image 🖼</button>
+        <button onClick={generateMemeImage} className="form--submit">Get a new meme image 🖼</button>
       </div>
     </main>
   )
